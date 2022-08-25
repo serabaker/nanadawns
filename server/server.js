5d225df68674e2c17aa82e71906c22ef5c6e27ed
Britny Lain <69497484+serabaker@users.noreply.github.com>
2022-08-24 23:25:45 -0500

    restarted

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const express = require('express');
+// const express = require('express');
 // const app = express();
 const port = process.env.Port || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
